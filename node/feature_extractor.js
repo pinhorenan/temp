768df3d7bbe2bfb76e88197dd2ea3d3eb7612d67
Renan Pinho <pinhorenan@outlook.com>
2026-02-11 06:52:01 -0300

    Implementação de um algoritmo de vizinho mais próximo simples.

--- node/feature_extractor.js
+++ node/feature_extractor.js
@@ -1,5 +1,5 @@
 const constants = require("../common/constants.js");
-const features = require("../common/features.js");
+const featureFunctions = require("../common/featureFunctions.js");
 
 const fs = require("fs");
 
@@ -11,10 +11,11 @@ for (const sample of samples) {
 	const paths = JSON.parse(
 		fs.readFileSync(constants.JSON_DIR + "/" + sample.id + ".json"),
 	);
-	sample.point = [features.getPathCount(paths), features.getPointCount(paths)];
+	const functions = featureFunctions.inUse.map((f) => f.function);
+	sample.point = functions.map((f) => f(paths));
 }
 
-const featureNames = ["Path Count", "Point Count"];
+const featureNames = featureFunctions.inUse.map((f) => f.name);
 
 fs.writeFileSync(
 	constants.FEATURES,
