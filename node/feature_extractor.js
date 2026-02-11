e2579ed0b763665291594bc5fed666c05d655918
Renan Pinho <pinhorenan@outlook.com>
2026-02-11 07:10:04 -0300

    Refinando a escala dos pontos.

--- node/feature_extractor.js
+++ node/feature_extractor.js
@@ -1,5 +1,6 @@
 const constants = require("../common/constants.js");
 const featureFunctions = require("../common/featureFunctions.js");
+const utils = require("../common/utils.js");
 
 const fs = require("fs");
 
@@ -14,6 +15,8 @@ for (const sample of samples) {
 	const functions = featureFunctions.inUse.map((f) => f.function);
 	sample.point = functions.map((f) => f(paths));
 }
+
+const minMax = utils.normalizePoints(samples.map((s) => s.point));
 
 const featureNames = featureFunctions.inUse.map((f) => f.name);
 
@@ -34,6 +37,13 @@ fs.writeFileSync(
 	constants.FEATURES_JS,
 	`const features=
    ${JSON.stringify({ featureNames, samples })}
+   ;`,
+);
+
+fs.writeFileSync(
+	constants.MIN_MAX_JS,
+	`const minMax=
+   ${JSON.stringify(minMax)}
    ;`,
 );
 
